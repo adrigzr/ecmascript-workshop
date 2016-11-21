76153d277fe2817052665e80b9ff3fd1594e4c3b
josex2r <codex2r@gmail.com>
2016-11-21 20:57:51 +0100

    fixed menu toggle

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	leftSideBarOpen: false
+	leftSideBarOpen: false,
+
+	actions: {
+		toggleMenu() {
+			this.toggleProperty('leftSideBarOpen');
+		}
+	}
 });
