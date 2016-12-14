b482dee7e5be2c86a05bf82bce6b732c9fabe480
Jose Luis Represa <joseluis.represa@bbva.com>
2016-12-14 16:55:05 +0100

    9

--- public/katas/ES6/09 - Proxies/code.js
+++ public/katas/ES6/09 - Proxies/code.js
@@ -2,4 +2,28 @@
 // Modificar la declaración del objeto `api` para hacer un proxy
 // al objeto `core` que prohiba el acceso a propiedad privadas, es decir,
 // propiedades que empiezan por `_`.
-const api = null;
+
+function isPublic(key) {
+	return key && key[0] !== '_';
+}
+
+const api = new Proxy(core, {
+	get(target, key) {
+		// Write here!
+	},
+	set(target, key, value) {
+		// Write here!
+	},
+	has(target, key) {
+		// Write here!
+	},
+	ownKeys(target) {
+		// Write here!
+	},
+	defineProperty(target, key, descriptor) {
+		// Write here!
+	},
+	deleteProperty(target, key) {
+		// Write here!
+	}
+});
