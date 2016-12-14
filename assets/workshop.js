763f18b6385a793ee71a4f3d85a07d9ae297c774
Adrián González Rus <adrian.gonzalez.rus@bbva.com>
2016-12-14 16:15:43 +0100

    Updated.

--- assets/workshop.js
+++ assets/workshop.js
@@ -256,7 +256,7 @@ define('workshop/components/mocha-runner', ['exports', 'ember'], function (expor
 
 		sandbox: 'allow-scripts allow-same-origin',
 
-		src: '/sandbox.html',
+		src: 'sandbox.html',
 
 		onEvent: null,
 
@@ -11395,7 +11395,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("workshop/app")["default"].create({"babel":{"presets":["es2017"],"plugins":["transform-decorators-legacy"]},"name":"workshop","version":"0.0.0+edf95533"});
+  require("workshop/app")["default"].create({"babel":{"presets":["es2017"],"plugins":["transform-decorators-legacy"]},"name":"workshop","version":"0.0.0+ede23e06"});
 }
 
 /* jshint ignore:end */
